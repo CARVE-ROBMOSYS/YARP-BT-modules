42e2cd950d7d265b4ec5bd9d2155e0fb6e89f29e
Jason Chevrie <jason.chevrie@iit.it>
2018-12-07 17:59:48 +0100

    Implement condition node BottleLocatedWithConfidenceX

--- scenarios/meta_conditions_module.cpp
+++ scenarios/meta_conditions_module.cpp
@@ -17,6 +17,9 @@
 //YARP imports
 #include <yarp/os/Network.h>    // for yarp::os::Network
 #include <yarp/os/LogStream.h>  // for yError()
+#include <yarp/os/Port.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/RpcClient.h>
 
 //behavior trees imports
 #include <include/tick_server.h>
@@ -26,7 +29,13 @@ using namespace yarp::os;
 class MetaConditions : public TickServer
 {
 public:
+
+    // blackboard
     yarp::os::Port blackboard_port; //todo make private
+
+    // object localization
+    yarp::os::BufferedPort<Bottle> blob_extractor_port; //todo make private
+    RpcClient classifier_port; //todo make private
 
 private:
     Bottle cmd, response;
@@ -34,10 +43,13 @@ private:
 public:
     ReturnStatus request_tick(const std::string& params = "") override
     {
+        Bottle paramsList;
+        paramsList.fromString(params);
+
         cmd.clear();
         response.clear();
         ReturnStatus ret;
-        if(params == "AtInvPose")
+        if(paramsList.get(0).asString() == "AtInvPose")
         {
             cmd.addString("get");
             cmd.addString("InvPose");
@@ -50,13 +62,16 @@ public:
 
             ret = BT_SUCCESS;
         }
-        else if( params == "BottleLocatedWithConfidenceX" )
+        else if(paramsList.get(0).asString() == "BottleLocatedWithConfidenceX")
         {
-            //we assume is always located located
-            // **TODO** Implement this
-            yError()<< "Node not yet implemented";
-            ret = BT_SUCCESS;
+            std::string objectName = "Bottle";
+            if(paramsList.size() > 1) objectName = paramsList.get(1).asString();
+            double confidence = 0.8;
+            if(paramsList.size() > 2) confidence = paramsList.get(2).asDouble();
+
+            ret = this->processObjectLocatedWithConfidenceX(objectName, confidence);
         }
+
 // Not used, we will tick directly the blackboard
 
 
@@ -117,6 +132,76 @@ public:
         yInfo() << "request_halt,  replying with " << a.toString(ret);
         return ret;
     }
+
+    ReturnStatus processObjectLocatedWithConfidenceX(const std::string objectName= "Bottle", double confidence = 0.5)
+    {
+        if (blob_extractor_port.getInputCount() < 1)
+        {
+            yError() << "No connection to object blob extractor module (lbpExtract)";
+            return BT_FAILURE;
+        }
+
+        Bottle *pBlobs = blob_extractor_port.read(false);
+        if (!pBlobs)
+        {
+            yError() << "No object detected";
+            return BT_FAILURE;
+        }
+
+        if (pBlobs->size() < 0)
+        {
+            yError() << "No object detected";
+            return BT_FAILURE;
+        }
+
+        if (classifier_port.getOutputCount() < 1)
+        {
+            yError() << "No connection to object classifier module (himrepClassifier)";
+            return BT_FAILURE;
+        }
+
+        Bottle cmd,reply;
+        cmd.addVocab(Vocab::encode("classify"));
+        Bottle &options=cmd.addList();
+        for (int i=0; i<pBlobs->size(); i++)
+        {
+            std::ostringstream tag;
+            tag<<"blob_"<<i;
+            Bottle &item=options.addList();
+            item.addString(tag.str());
+            item.addList()=*(pBlobs->get(i).asList());
+        }
+
+        classifier_port.write(cmd,reply);
+
+        for (int i=0; i<pBlobs->size(); i++)
+        {
+            std::ostringstream tag;
+            tag<<"blob_"<<i;
+            if(!reply.check(tag.str()))
+            {
+                yError() << "Invalid answer from object classifier module (himrepClassifier)";
+                return BT_FAILURE;
+            }
+
+            Bottle *objectList = reply.find(tag.str()).asList();
+            if(!objectList->check(objectName))
+            {
+                yError() << objectName << "is missing in object database";
+                return BT_FAILURE;
+            }
+
+            double objectScore = reply.find(objectName).asDouble();
+            if(objectScore > confidence)
+            {
+                return BT_SUCCESS;
+            }
+        }
+
+        yInfo() << objectName << "not found, or found with insufficient confidence";
+
+        return BT_FAILURE;
+    }
 };
 
 int main(int argc, char * argv[])
@@ -132,7 +217,8 @@ int main(int argc, char * argv[])
     MetaConditions skill;
     skill.configure_tick_server("/metaconditions");
     skill.blackboard_port.open("/metaconditions/blackboard/rpc:o");
-
+    skill.blob_extractor_port.open("/metaconditions/BottleLocalization/blobs:i");
+    skill.classifier_port.open("/metaconditions/BottleLocalization/classifier/rpc:o");
 /*
     std::cout << "Action ready. To send commands to the action, open and type: yarp rpc /metaconditions/tick:i,"
               <<" then type help to find the available commands "
